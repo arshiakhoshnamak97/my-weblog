4602b1cf197dce2449a07805b65328f147f7d331
Arshia khoshnamak <arshiakhoshnamak97@gmail.com>
2026-02-15 16:59:19 +0330

    posts by category

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,6 @@
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include 
 from . import views
 from  django.conf.urls.static import static
 from django.conf import settings
@@ -9,4 +9,5 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("",views.home),
+    path("category/",include("blogs.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- blogs/views.py
+++ blogs/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
-
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponse
+from .models import Blog,Category
 # Create your views here.
+
+
+def posts_by_category(request,category_id):
+    posts=Blog.objects.filter(status="Published",category=category_id)
+    
+    category=get_object_or_404(Category,pk=category_id)
+    
+    context={"posts": posts,
+             "category":category}
+    return render(request,"posts_by_category.html",context)
